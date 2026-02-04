cc4a072fb1ea00ab7d94ddf969074c391dfdf237
MSI-90 <s.miller_22@outlook.com>
2026-02-04 20:03:55 +0700

    Добавлен компонент Button и компонент AboutModal с информацией о проекте.

--- my-app/src/widgets/LayoutHeader/Header.tsx
+++ my-app/src/widgets/LayoutHeader/Header.tsx
@@ -1,10 +1,44 @@
+import Button from '../../shared/components/Button';
+import AboutModal from "../../features/about-project/ui/AboutModal";
+import {useState} from 'react'
+import './Header.css';
 
+interface IDialogInfo {
+    dialogTitle: string;
+    dialogBody: string;
+}
 
 export default function Header(){
+    const [dialog, setDialog] = useState(false);
+    const dialogInfo: IDialogInfo = {
+        dialogTitle: 'О проекте',
+        dialogBody: 'Демонстрация диалога'
+    }
+
     return (
         <>
             <header className="header-container">
-                <h1 className="header-title">Header страницы</h1>
+                <h2 className="header-title">Header страницы</h2>
+                <Button
+                    baseButton={true}
+                    onClick={() =>
+                        setDialog(true)
+                    }
+                >
+                    О проекте
+                </Button>
+
+                {dialog && (
+                    <AboutModal
+                        header={dialogInfo.dialogTitle}
+                        modal={true}
+                        onAction={(type) => {
+                            if(type === 'dismiss') setDialog(false)
+                        }}
+                    >
+                        {dialogInfo.dialogBody}
+                    </AboutModal>
+                )}
             </header>
         </>
     )
