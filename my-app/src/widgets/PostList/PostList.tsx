e518304a3691f253297e80b7baffa7b5d4208bdf
MSI-90 <s.miller_22@outlook.com>
2026-02-05 19:18:11 +0700

    Докинул посты заглушки в PostList.tsx

--- my-app/src/widgets/PostList/PostList.tsx
+++ my-app/src/widgets/PostList/PostList.tsx
@@ -2,7 +2,13 @@ import PostCard from "../../entities/post/ui/PostCard.tsx";
 
 const posts = [
     { id: 1, title: 'Первый пост', body: 'Текст поста' },
-    { id: 2, title: 'Второй пост', body: 'Ещё текст' }
+    { id: 2, title: 'Второй пост', body: 'Текст второго поста' },
+    { id: 3, title: 'Третьй пост', body: 'Текст третьего поста' },
+    { id: 4, title: 'Четвертый пост', body: 'Текст четвертого поста' },
+    { id: 5, title: 'Пятый пост', body: 'Текст пятого поста' },
+    { id: 6, title: 'Шестой пост', body: 'Текст шестого поста' },
+    { id: 7, title: 'Седьмой пост', body: 'Текст седьмого поста' },
+    { id: 8, title: 'Восьмой пост', body: 'Текст восьмого поста' },
 ]
 
 export default function PostList() {
