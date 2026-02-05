ccb143d0e65e0523353b0528a051dc3b584e60e9
MSI-90 <s.miller_22@outlook.com>
2026-02-05 19:09:32 +0700

    (update:) Компонент модального онка был обновлен с учетом подхода Compound Components: теперь компонент Modal может принимать дочерние компоненты Modal.Header, Modal.Body и Modal.Footer Интерфейс IModalProps был дополнен и все типы в нем изменены как optional.

--- my-app/src/shared/ui/Modal/Modal.tsx
+++ my-app/src/shared/ui/Modal/Modal.tsx
@@ -2,33 +2,53 @@ import Button from '../Buttons/Button';
 import './Modal.css';
 
 interface IModalProps {
-    title: string;
-    body: string;
-    onClick: () => void;
+  title?: string;
+  body?: string;
+  onClick?: () => void;
+  children?: React.ReactNode[];
 }
 
-export default function Modal ({ title, body, onClick }:IModalProps) {
+export default function Modal (props: IModalProps) {
+  return (
+    <>
+      <div className="modal-overlay" onClick={props?.onClick}>
+        <div className="modal-window" onClick={(e) => e.stopPropagation()}>
+          {props?.children}
+        </div>
+      </div>
+    </>
+  )
+}
 
-    return (
-        <>
-            <div className="modal-overlay" onClick={onClick}>
-                <div className="modal-window" onClick={(e) => e.stopPropagation()}>
-                    <div className="modal-header">
-                        <h3>{title}</h3>
-                        <button className="modal-close" onClick={onClick}>×</button>
-                    </div>
+Modal.Header = function ModalHeader (props: IModalProps) {
+  return (
+    <>
+      <div className="modal-header">
+        <h3>{props?.title}</h3>
+        <button className="modal-close" onClick={props?.onClick}>×</button>
+      </div>
+    </>
+  )
+}
 
-                    <div className="modal-body">
-                        {body}
-                    </div>
+Modal.Body = function ModalBody (props: IModalProps) {
+  return (
+    <>
+      <div className="modal-body">
+        {props?.body}
+      </div>
+    </>
+  )
+}
 
-                    <div className="modal-footer">
-                        <Button baseButton={true} onClick={onClick}>
-                            Закрыть
-                        </Button>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+Modal.Footer = function ModalFooter (props: IModalProps) {
+  return (
+    <>
+      <div className="modal-footer">
+        <Button baseButton={true} onClick={props?.onClick}>
+          Закрыть
+        </Button>
+      </div>
+    </>
+  )
 }
--- my-app/src/widgets/LayoutHeader/Header.tsx
+++ my-app/src/widgets/LayoutHeader/Header.tsx
@@ -6,40 +6,39 @@ import {createPortal} from 'react-dom';
 import {useState} from "react";
 
 interface IAboutModalInfo {
-    title: string;
-    body: string;
+  title: string;
+  body: string;
 }
 
 export default function Header(){
-    const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-    const aboutModalInfo: IAboutModalInfo = {
-        title: 'О приложении',
-        body: 'Тело модального окна',
-    }
+  const aboutModalInfo: IAboutModalInfo = {
+    title: 'О приложении',
+    body: 'Тело модального окна',
+  }
 
-    return (
-        <>
-            <header className="header-container">
-                <h2 className="header-title">Header страницы</h2>
-                <ThemeSwitcher />
-                <Button
-                    baseButton={true}
-                    onClick={() => setShowModal(true)}
-                >
-                    О проекте
-                </Button>
+  return (
+    <>
+      <header className="header-container">
+        <h2 className="header-title">Header страницы</h2>
+        <ThemeSwitcher />
+        <Button
+          baseButton={true}
+          onClick={() => setShowModal(true)}
+        >
+          О проекте
+        </Button>
 
-                {showModal && createPortal(
-                    <Modal
-                        {...aboutModalInfo}
-                        onClick={()=>
-                            setShowModal(false)
-                    }
-                    />,
-                    document.body)
-                }
-            </header>
-        </>
-    )
+        {showModal && createPortal(
+          <Modal onClick={()=> setShowModal(false)}>
+            <Modal.Header title={aboutModalInfo.title} onClick={()=>setShowModal(false)} />
+            <Modal.Body body={aboutModalInfo.body} />
+            <Modal.Footer onClick={()=>setShowModal(false)}/>
+          </Modal>,
+          document.body)
+        }
+      </header>
+    </>
+  )
 }
